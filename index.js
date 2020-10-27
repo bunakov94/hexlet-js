2027fbb8a44b58a75b73fe9277bd8626edd74c76
bunakov94 <bunakov.code@gmail.com>
2020-10-27 14:51:58 +0300

    add prettier

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var _ = require('lodash');
+var _ = require("lodash");
 
-console.log(_.last(['one', 'two']));
-console.log(_.partition([1, 2, 3, 4], n => n % 2));
+console.log(_.last(["one", "two"]));
+console.log(_.partition([1, 2, 3, 4], (n) => n % 2));
